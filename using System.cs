b8476ec1199c5cf3955bcf18474a28a06ce52afd
Edimar Morales <66224778+edimarmorales@users.noreply.github.com>
2021-03-02 10:07:24 -0400

    Add files via upload

---
+++ using System.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Números
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 20;
+			int c = 7;
+
+			Console.WriteLine();
+
+			Console.Write(a);
+			Console.Write(a);
+			Console.Write(c);
+			Console.Write(b);
+
+			Console.WriteLine();
+			Console.WriteLine();
+
+
+			Console.WriteLine("__________________________________________________________________");
+			Console.WriteLine();
+
+			Console.Write(b);
+			Console.Write(c);
+			Console.Write(a);
+
+			Console.WriteLine();
+			Console.WriteLine();
+
+			Console.WriteLine("__________________________________________________________________");
+			Console.WriteLine();
+
+			Console.Write(b);
+			Console.Write(c);
+			Console.Write(b);
+			Console.Write(c);
+
+			Console.ReadKey();
+
+		}
+	}
+}
